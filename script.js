3dc72019276e6f02792dd6300ebae77f00eff3ee
Irvin Barreto <irvinbarreto07@gmail.com>
2021-03-17 20:51:00 -0600

    Se agrego el efecto de typeado en el portafolio

--- script.js
+++ script.js
@@ -1,51 +1,69 @@
-((d)=>{
-    const $btnMenu = d.querySelector(".menu-btn");
-    const $menu = d.querySelector(".menu");
-    $btnMenu.addEventListener("click", e =>{
-        $btnMenu.firstElementChild.classList.toggle("none");
-        $btnMenu.lastElementChild.classList.toggle("none");
-        $menu.classList.toggle("is-active")
-    })
-    d.addEventListener("click", e=>{
-        if(!e.target.matches(".menu a")) return false;
-            $btnMenu.firstElementChild.classList.remove("none");
-            $btnMenu.lastElementChild.classList.add("none");
-            $menu.classList.remove("is-active");
-    })
+((d) => {
+  const $btnMenu = d.querySelector(".menu-btn");
+  const $menu = d.querySelector(".menu");
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
 })(document);
 
+((d) => {
+  const $form = d.querySelector(".contact-form");
+  const $loader = d.querySelector(".contact-form-loader");
+  const $response = d.querySelector(".contact-modal-response");
 
-((d)=>{
-    const $form = d.querySelector(".contact-form")
-    const $loader = d.querySelector(".contact-form-loader")
-    const $response = d.querySelector(".contact-modal-response")
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/f5c55ca3d1eee46bc32cb5f4331c6b7d", {
+      method: "POST",
 
-    $form.addEventListener("submit",(e)=>{
-        e.preventDefault();
-        $loader.classList.remove("none");
-        fetch("https://formsubmit.co/ajax/f5c55ca3d1eee46bc32cb5f4331c6b7d",{
-            method:"POST",
-            
-            body: new FormData(e.target)
-            // mode: "no-cors"
-        })
-        .then((res) => (res.ok ? res.json(): Promise.reject(res)))
-        .then((json) => {
-            console.log(json);
-            
-            location.hash = "#gracias";
-            $form.reset();
-        })
-        .catch(err =>{
-            console.log(err);
-            let message = err.statusText || "Ocurrio un error al enviar, intenta nuevamente"
-            $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
-            
-        }).finally(()=>{
-            $loader.classList.add("none");
-            setTimeout(()=>{
-                location.hash = "#close"
-            },3000)
-        });
+      body: new FormData(e.target),
+      // mode: "no-cors"
     })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrio un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
 })(document);
+
+const typed = new Typed(".typed", {
+  /* strings: ['Hola mucho gusto', 'Soy desarrollador web'], */
+  stringsElement: "#cadenas-texto", // ID del elemento que contiene cadenas de texto a mostrar.
+  typeSpeed: 150, // Velocidad en milisegundos para poner una letra,
+  startDelay: 500, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+  backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+  smartBackspace: false, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+  shuffle: false, // Alterar el orden en el que escribe las palabras.
+  backDelay: 2500, // Tiempo de espera despues de que termina de escribir una palabra.
+  loop: true, // Repetir el array de strings
+  loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+  showCursor: true, // Mostrar cursor palpitanto
+  cursorChar: "|", // Caracter para el cursor
+  contentType: "html", // 'html' o 'null' para texto sin formato
+});
